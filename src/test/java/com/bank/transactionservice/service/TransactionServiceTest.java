8b990a68835c6585a3a5adad0bf442c3e73c1d82
Raul <raul.giblak98@e-uvt.ro>
2025-02-15 16:55:52 +0200

    v1_transactionservice:

--- src/test/java/com/bank/transactionservice/service/TransactionServiceTest.java
+++ src/test/java/com/bank/transactionservice/service/TransactionServiceTest.java
@@ -5,9 +5,7 @@ import com.bank.transactionservice.repository.AccountRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
